23084caabe1d57ddf8816f35e8afe0a3b8e791f4
Justin Petersen <justin@justinpetersen.com>
2012-07-18 20:06:53 -0700

    M.

--- js/client.js
+++ js/client.js
@@ -30,24 +30,6 @@ function WormTvClient( ) {
 	
 	this.initYouTubePlayer = function( ) {
 		
-		var tag = document.createElement('script');
-		      tag.src = "//www.youtube.com/player_api";
-		      var firstScriptTag = document.getElementsByTagName('script')[0];
-		      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-		
-		var player;
-		function onYouTubePlayerAPIReady() {
-		  player = new YT.Player('player', {
-		    height: '390',
-		    width: '640',
-		    videoId: 'u1zgFlCw8Aw',
-		    events: {
-		      'onReady': this.onPlayerReady,
-		      'onStateChange': this.onPlayerStateChange
-		    }
-		  });
-		}
-		
 	};
 	
 	this.facebookLogin = function( ) {
