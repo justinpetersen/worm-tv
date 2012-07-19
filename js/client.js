b20d8f709e563fb38cc5ef3d5db138303b0d050b
Justin Petersen <justin@justinpetersen.com>
2012-07-18 19:53:09 -0700

    Added YouTube API script tag

--- js/client.js
+++ js/client.js
@@ -30,6 +30,11 @@ function WormTvClient( ) {
 	
 	this.initYouTubePlayer = function( ) {
 		
+		var tag = document.createElement('script');
+		      tag.src = "//www.youtube.com/player_api";
+		      var firstScriptTag = document.getElementsByTagName('script')[0];
+		      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+		
 		var player;
 		function onYouTubePlayerAPIReady() {
 		  player = new YT.Player('player', {
